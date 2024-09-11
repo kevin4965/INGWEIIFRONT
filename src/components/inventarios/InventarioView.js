c160523f3533e78ee00d798f344776ed524469bb
jorgejc <jorgejc25@gmail.com>
2024-09-10 23:57:21 -0500

    front con navegación

---
+++ src/components/inventarios/InventarioView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export const InventarioView = () => {
+    
+  return (
+    <div>
+      InventarioView
+    </div>
+  )
+}
+
---
+++ src/components/tipos/TipoView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const TipoView = () => {
+  return (
+    <div>
+      TipoView
+    </div>
+  )
+}
+
